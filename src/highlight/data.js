0044ed682c77f56c7ad8bdca8835611994c40fe0
Ashkaan Jaberi <ashkaan14@gmail.com>
2018-09-27 16:21:10 +0000

    style guide 1.0 done

---
+++ src/highlight/data.js
@@ -0,0 +1,59 @@
+module.exports = {
+  one: [
+    {
+      startOffset: 4,
+      endOffset: 31,
+      color: '#45b5e6', //blue
+      priority: 0,
+    },
+    {
+      startOffset: 28,
+      endOffset: 49,
+      color: '#fa8926',  //orange 
+      priority: 1,
+    },
+    {
+      startOffset: 58,
+      endOffset: 70,
+      color: '#e8e8e8',  //grey 
+      priority: 2,
+    },
+    {
+      startOffset: 65,
+      endOffset: 80,
+      color: '#97c728', //green
+      priority: 1,
+    },
+  ],
+  
+  two: [
+    {
+      startOffset: 0,
+      endOffset: 31,
+      color: '#45b5e6', //blue
+      priority: 2,
+    },
+    {
+      startOffset: 28,
+      endOffset: 49,
+      color: '#fa8926',  //orange 
+      priority: 1,
+    },
+    {
+      startOffset: 38,
+      endOffset: 70,
+      color: '#e8e8e8',  //grey 
+      priority: 0,
+    },
+    {
+      startOffset: 65,
+      endOffset: 80,
+      color: '#97c728', //green
+      priority: 1,
+    },
+  ],
+};
+
+
+// overlap priorities ?
+// test out more data types 
--- src/highlight/index.js
+++ src/highlight/index.js
@@ -1,81 +1,79 @@
+const highlights = require('./data.js');
+
 let string = 'You will deliver new technology with an adorable puppy. Perfect!';
 
-const highlights = [
-  {
-    startOffset: 4,
-    endOffset: 31,
-    color: '#45b5e6', //blue
-    priority: 0,
-  },
-  {
-    startOffset: 28,
-    endOffset: 49,
-    color: '#fa8926',  //orange 
-    priority: 1,
-  },
-  {
-    startOffset: 58,
-    endOffset: 70,
-    color: '#e8e8e8',  //grey 
-    priority: 2,
-  },
-  {
-    startOffset: 65,
-    endOffset: 80,
-    color: '#97c728', //green
-    priority: 1,
-  },
-];
 
-// this will contain the master style guide for every chunk of the string
-let styleGuide = [];  
-
-//iterate through all the rules given in 'highlights'
-for(let i=0; i < highlights.length; i++){
-  let current = highlights[i];
-  let previous = styleGuide[styleGuide.length -1];
-
-  // deals with the very first highlight rule we get - accounts for space before
-  if(i === 0) {
-    let difference = current.startOffset - 0;
-    if(difference > 0) {
-      let offset = difference - 1;
-      styleGuide.push({
-        startOffset: 0,
-        endOffset: offset,
-        priority: null,
-      });
-
-      styleGuide.push(current);
-    }
-  }
-
-  if(i > 0) {
-    let difference = current.startOffset - previous.endOffset;
-    if(difference < 0) {
-      if(current.priority > previous.priority){
-        current.startOffset -= difference - 1;
+let createStyleGuide = (highlights) => {
+  
+  // this will be the master style guide for every offset of the string
+  let styleGuide = [];  
+  
+  // iterating through all the rules given in 'highlights' array
+  for(let i=0; i < highlights.length; i++){
+    let current = highlights[i];  
+    let previous = styleGuide[styleGuide.length -1];
+  
+    // deals with the very first highlight rule we get since previous = undefined
+    if(i === 0) {
+      let difference = current.startOffset - 0;
+      
+      // are there non highlighted offsets before our first rule? 
+      if(difference > 0) {
+        let endOffset = difference - 1;
+        styleGuide.push({
+          startOffset: 0,
+          endOffset: endOffset,
+          priority: null,
+        });
         styleGuide.push(current);
-      } else if (current.priority < previous.priority) {
-        previous.endOffset -= difference -1;
-        styleGuide.pop();
-        styleGuide.push(previous);
+      }
+      
+      if(difference === 0) {
+        styleGuide.push(current);
+      }
+    }
+  
+    // deals with all other highlight rules after the first
+    if(i > 0) {
+      let difference = current.startOffset - previous.endOffset;
+      
+      // handling offset overlaps
+      if(difference <= 0) {
+        if(current.priority > previous.priority){
+          current.startOffset -= difference - 1;
+          styleGuide.push(current);
+        } 
+        if (current.priority < previous.priority) {
+          // could add a check for the edge case that a previous rule would be completely overlapped / replaced
+          previous.endOffset += difference - 1;
+          styleGuide.push(current);
+        }
+        
+        if(current.priority === previous.priority){
+          // deal with this edge case later
+        }
+      } 
+      
+      // handling no offset overlaps 
+      if(difference > 0) {
+        let endOffset = current.startOffset - 1;
+        let startOffset = previous.endOffset + 1;
+        styleGuide.push({
+          startOffset: startOffset,
+          endOffset: endOffset,
+          priority: null,
+        });
         styleGuide.push(current);
       } 
-    } 
-    if(difference > 0) {
-      let endOffset = current.startOffset - 1;
-      let startOffset = previous.endOffset + 1;
-      styleGuide.push({
-        startOffset: startOffset,
-        endOffset: endOffset,
-        priority: null,
-      });
-      styleGuide.push(current);
-    } 
-
+  
+    }
   }
+  
+  return styleGuide;
+};
 
-}
 
-console.log(styleGuide);
+// console.log(createStyleGuide(highlights.one));
+// console.log(createStyleGuide(highlights.two));
+
+
